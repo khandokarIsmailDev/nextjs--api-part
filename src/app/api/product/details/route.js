2c536eee118b48cb9b3c1331ef447c674e2869d3
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-02-21 22:47:18 +0600

    only api

---
+++ src/app/api/product/details/route.js
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function GET(req,res){
+    try{
+
+        let {searchParams} = new URL(req.url)
+        let id = parseInt(searchParams.get('id'))
+        const prisma = new PrismaClient() 
+        const result = await prisma.products.findUnique({
+            where:{id:id},
+            include:{product_details:true}
+        })
+
+        return NextResponse.json({status:"sucess",data:result})
+    }catch(error){
+        return NextResponse.json({status:"fail",data:error.toString()})
+    }
+}
